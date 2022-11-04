84f1d61a4107988918117be93b951fd78fdd0e02
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2022-11-04 18:34:46 +0100

    Create Gatsby project
    Created using `gatsby new` with settings largely unchanged.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mark Avenue Website
+
+Powered by [Gatsby].
+
+[Gatsby]: https://www.gatsbyjs.com/
---
+++ gatsby-config.ts
@@ -0,0 +1,15 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Mark Avenue`,
+    siteUrl: `https://www.markavenue.sk`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: false,
+  plugins: ["gatsby-plugin-postcss"]
+};
+
+export default config;
