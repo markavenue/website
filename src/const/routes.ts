2c61dc45895c07bbc3f20570adef22e82f148f3a
Samuel Mitas <samomitas@gmail.com>
2022-12-02 18:30:52 +0100

    Optimize hero sections for small devices

---
+++ src/const/routes.ts
@@ -0,0 +1,6 @@
+export const HOMEPAGE = '/';
+
+export const APARTMEA_PAGE = '/apartmea';
+export const FRANGO_PAGE = '/frango';
+export const HATE_FREE_ZONE_PAGE = '/hate-free-zone';
+export const SOUPCULTURE_PAGE = '/soupculture';
