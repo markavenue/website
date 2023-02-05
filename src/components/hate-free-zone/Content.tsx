5ce892cf31a031a54dec8eaff93d82c8d1c33908
Samuel Mitas <samomitas@gmail.com>
2023-02-05 16:59:31 +0100

    Fix eslint error

--- src/components/hate-free-zone/Content.tsx
+++ src/components/hate-free-zone/Content.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
 import Article from '../text/article/Article';
 import Description from '../text/article/Description';
 import Paragraph from '../text/article/Paragraph';
