8269c48fb10efcfd6ad58834d6966fddffc00110
Ľubomír Kučera <lubomir.kucera.jr@gmail.com>
2023-07-05 21:51:53 +0200

    Load analytics-related scripts as the last

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,43 +1,53 @@
 import { PreRenderHTMLArgs, RenderBodyArgs } from 'gatsby';
 import React from 'react';
 
-export function onRenderBody({
-  setHeadComponents,
-  setHtmlAttributes,
-}: RenderBodyArgs) {
+export function onRenderBody({ setHtmlAttributes }: RenderBodyArgs) {
   setHtmlAttributes({
     lang: 'sk',
   });
+}
 
-  setHeadComponents([
+export function onPreRenderHTML({
+  getHeadComponents,
+  getPostBodyComponents,
+  replaceHeadComponents,
+  replacePostBodyComponents,
+}: PreRenderHTMLArgs) {
+  if (process.env.NODE_ENV === 'development') {
+    return;
+  }
+
+  const oldHeadComponents = getHeadComponents();
+  const newHeadComponents: React.ReactNode[] = [];
+  const postBodyComponents = [
+    ...getPostBodyComponents(),
     <script
       id="cookieyes"
       key="cookieyes"
       src="https://cdn-cookieyes.com/client_data/40f34866b5b77c07eab5de16/script.js"
       type="text/javascript"
     />,
-  ]);
-}
+  ];
 
-export function onPreRenderHTML({
-  getHeadComponents,
-  replaceHeadComponents,
-}: PreRenderHTMLArgs) {
-  const headComponents = getHeadComponents();
+  oldHeadComponents.forEach((item) => {
+    const { key } = item as React.ReactElement;
 
-  headComponents.sort((item) =>
-    (item as React.ReactElement).key === 'cookieyes' ? -1 : 1,
-  );
-  if (process.env.NODE_ENV !== 'development') {
-    const gtagIndex = headComponents.findIndex(
-      (item) =>
-        (item as React.ReactElement).key === 'gatsby-plugin-google-gtag',
-    );
-    headComponents[gtagIndex] = React.cloneElement(
-      headComponents[gtagIndex] as React.ReactElement,
-      { 'data-cookieyes': 'cookieyes-analytics' },
-    );
-  }
+    if (
+      typeof key === 'string' &&
+      key.startsWith('gatsby-plugin-google-gtag')
+    ) {
+      postBodyComponents.push(
+        key === 'gatsby-plugin-google-gtag'
+          ? React.cloneElement(item as React.ReactElement, {
+              'data-cookieyes': 'cookieyes-analytics',
+            })
+          : item,
+      );
+    } else {
+      newHeadComponents.push(item);
+    }
+  });
 
-  replaceHeadComponents(headComponents);
+  replaceHeadComponents(newHeadComponents);
+  replacePostBodyComponents(postBodyComponents);
 }
